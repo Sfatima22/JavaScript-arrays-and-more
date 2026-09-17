f86817db6eef7b589b957c825081102bfd6383f5
Sfatima22 <shaguftaf28@gmail.com>
2026-09-17 11:59:32 +0000

    arrays

---
+++ 03.js
@@ -0,0 +1,11 @@
+// const arr=[1,2,3,4];
+// console.log(Array.isArray(arr));
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers.unshift(0) //to add element at start of array
+// console.log(numbers);
+numbers.push(11) //to add elements at end of an array
+// console.log(numbers);
+numbers.shift(); //removes the first element of array
+// console.log(numbers);
+numbers.pop() //removes last element of an array
+console.log(numbers);
