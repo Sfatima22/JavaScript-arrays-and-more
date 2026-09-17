eb73be4e93ed64f70e9d3d97d1524f9f19308f64
Sfatima22 <shaguftaf28@gmail.com>
2026-09-17 13:00:36 +0000

    arrays

---
+++ 06.js
@@ -0,0 +1,18 @@
+// let first="sam";
+// console.log(first);
+// let second =first;
+// first ="seen";
+// console.log(first);
+
+let obj1 = {
+    hair: "black",
+    eyes: "brown"
+}
+console.log(obj1);
+
+let obj2 = obj1
+console.log(obj2);
+
+obj1.sleeps = true; //adds new element to obj 1
+console.log(obj2);
+console.log(obj1);
