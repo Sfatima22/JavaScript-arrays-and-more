6bf78b94f0b568f39215f2606c1da545c479d412
Sfatima22 <shaguftaf28@gmail.com>
2026-09-17 11:15:29 +0000

    arrays

---
+++ 01.js
@@ -0,0 +1,10 @@
+// const array1=["eat", "sleep"];
+// const array2= new Array("eat","sleep");
+// console
+// const newData = [
+//     {'task1': 'exercise'},
+//     [1, 2 ,3]]
+//     function hello() { console.log('hello')}
+
+// const arr=["hey","history","you"];
+// console.log(arr[2]);
