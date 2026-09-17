58aa4167c10a032f143d5ef3ecc10107a932a6b4
Sfatima22 <shaguftaf28@gmail.com>
2026-09-17 11:52:45 +0000

    arrays

---
+++ 02.js
@@ -0,0 +1,10 @@
+// let arr = ['h', 'e'];
+// let arr1 = arr;
+// arr1.push('l');
+
+// console.log(arr);
+let arr = ['h', 'e'];
+arr.name = 'John';
+
+console.log(arr); // ["h", "e"]
+console.log(arr.name); // "John"
