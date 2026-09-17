58acb4bf2bad3423a5a66c7661f953deb2b6599b
Sfatima22 <shaguftaf28@gmail.com>
2026-09-17 12:05:05 +0000

    arrays

---
+++ 05.js
@@ -0,0 +1,14 @@
+const arr1 = [1, 2, 3]
+arr1.name = "Hey"
+arr1.firstname = 'fariya'
+arr1.lastname = 'fatima'
+arr1.age = 20
+arr1.ok = ['h1', 'h3']
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+console.log(arr1[3]);
+console.log(arr1[4]);
+
+console.log(arr1);
+console.log(arr1.length);
