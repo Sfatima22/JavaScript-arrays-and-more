b392c2eb859e2d71e37eac34ff76055358614ecc
Sfatima22 <shaguftaf28@gmail.com>
2026-09-18 07:14:18 +0000

    arrays

---
+++ 10.js
@@ -0,0 +1,7 @@
+// let arr=[0,9,7,5];
+// arr=[]; //empty an aray
+// console.log(arr);
+
+
+const arr=[9,6,3,];
+console.log(arr.length);
---
+++ 11.js
@@ -0,0 +1,9 @@
+let arr=[["farah",50]["khan",80]]; //two dimensional/multi array / array inside array
+console.log(arr);
+
+let person1=["kim","pink"]
+let person2=["kay","green"]
+let person3=["kash","brown"]
+
+let personData=[person1,person2,person3];
+console.log(personData);
