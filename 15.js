5f51f5391adcb2164e9064f892d2e67bf494948b
Sfatima22 <shaguftaf28@gmail.com>
2026-09-18 07:51:37 +0000

    arrays

---
+++ 15.js
@@ -0,0 +1,5 @@
+const code=[1,2,3,4];
+let final=code.copyWithin(2,1); // 2 is the targer and 1 is start target
+//  is the index from where the elemnt is to be 
+// copied and start is the index of elemnt where it is to be pasted
+console.log(final);
