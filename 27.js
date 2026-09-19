6f322870c21b3b145e6427a997c60354f10442e9
Sfatima22 <shaguftaf28@gmail.com>
2026-09-19 12:26:33 +0000

    arrays

---
+++ 27.js
@@ -0,0 +1,19 @@
+// const arr1=["keys","mouse","screen","pen","remote"];
+// console.log(arr.at(3));  //at finds the value at the given index
+
+
+
+const arr2=[1,2,3,4,5,6];
+// console.log(arr1.concat("here", arr2)) //attaching two arrays and adding here in between 
+
+// console.log(arr1.copyWithin(1, 2, 4));  //  copying values
+
+
+// console.log(arr1.every((e) => e%2==0)); //every() checks if it is true or false
+
+// console.log(arr1.fill(0,2,4));
+// console.log(arr2.fill(0,2,4));/ fill the given element in an array puts 0 at 2 index
+
+
+
+// console.log(arr2.findIndex((x) => x % 2 == 0));  finds the value and gives the index
