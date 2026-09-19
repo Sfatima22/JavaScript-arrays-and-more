4e9e9ff37b5829ab54e8b2c23815316e2e832468
Sfatima22 <shaguftaf28@gmail.com>
2026-09-19 09:28:34 +0000

    arrays

---
+++ 24.js
@@ -0,0 +1,8 @@
+let arr=["k", "p","h","b"];
+console.log(arr.includes("h"));
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(arr.includes(11));
+
+// checks if the elemnt is there in array
---
+++ 25.js
@@ -0,0 +1,18 @@
+// let arr=[1,2,3,4,5,6,7];
+// console.log(arr.indexOf(90)); //finds index of an elemnt
+
+// -----------------------------------------------//
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const arr2 = arr;
+
+// console.log(arr2 === arr);
+
+// const s = "Hello";
+// const j = s;
+// console.log(j === s);
+const arr3 = new Array(1, 2, 3, 4, 4, 5);
+console.log((arr == arr3));
+console.log(typeof arr);
+console.log(typeof arr3);
