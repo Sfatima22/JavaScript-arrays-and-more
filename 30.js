8df9aac76fa213288b7e027d0004e845332440da
Sfatima22 <shaguftaf28@gmail.com>
2026-09-20 19:03:33 +0000

    object methods

---
+++ 30.js
@@ -0,0 +1,26 @@
+let details={
+    name:"fatima",
+    age:21
+}
+
+// let more={
+//     hobbies :"baking"
+
+// }
+//  Object.assign(details, more); copies from one obj  to another
+//  console.log(details);
+
+
+// let create=Object.create(details);
+// console.log(create); 
+// create.study="b.tech" 
+// console.log(create) creates a new object retuens empty but has access nto all the values in the obj 
+
+
+// let obj ={};
+
+// Object.defineProperty(obj, "name", {  used to create a proprty inside an object and controls how it b ehaves
+//   value : "fatima"
+// })
+// console.log(obj.name);
+
