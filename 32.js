033c29b34d3f81283bb290909d1bb40b0771443c
Sfatima22 <shaguftaf28@gmail.com>
2026-09-22 05:03:06 +0000

    string methods

--- 32.js
+++ 32.js
@@ -1,3 +1,23 @@
 // let name="fatima";
 // console.log(name.charAt(2)); 
 
+// let names = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// let result = names.charCodeAt(0);
+
+// console.log(result);
+// for (let i = 0; i <= names.length - 1; i++) {
+//   let out = names.charCodeAt(i);
+//   console.log(out);
+// }
+
+// console.log(name.charCodeAt(3)) gives unicode
+
+
+// let result = names.codePointAt(0);
+// console.log(result);
+
+
+// let str1="hey";
+// let str2="there";
+// console.log(str1.concat(" ", str2)); attaches two string 
